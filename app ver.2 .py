742a0a7ef1f287e4996ae93bcf0b268603286737
REIKA1224 <120627947+REIKA1224@users.noreply.github.com>
2025-12-09 16:54:09 +0900

    Update app ver.2 .py

--- app ver.2 .py
+++ app ver.2 .py
@@ -115,8 +115,8 @@ if submit_btn:
                 st.success("命名案が完成しました！")
                 st.markdown("### 📝 提案結果と分析")
                 
-                # 青い枠で表示
-                st.info(response_content)
+                with st.container(border=True):
+    st.write(response_content)
 
                 # ------------------------------
                 # 生成結果をCSVに保存
@@ -146,6 +146,7 @@ if submit_btn:
 st.markdown("---")  # 区切り線を表示
 st.markdown("### 評価アンケートはこちら")
 st.markdown("[👉 Googleフォームで評価する](https://www.amazon.co.jp/)")
+
 
 
 
